80bc040a563c00049bd621f8654ed958341854c3
haritha037 <e20037@eng.pdn.ac.lk>
2025-11-20 19:35:55 +0530

    Add CorsConfiguration

---
+++ backend/src/main/java/com/haritha/backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.haritha.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")  // apply CORS to all endpoints
+                        .allowedOrigins("http://localhost:5173")   // Vite frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
